3d76432d8ccf949b36bf1deaa11b518dea956df0
JanGursky <jgursk@gmail.com>
2024-03-19 13:31:37 +0100

    Models and Controllers user corrected

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,40 @@
 <?php
-
 namespace App\Models;
-
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class User extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
     public function role()
     {
@@ -48,4 +75,5 @@ class User extends Model
     {
         return $this->hasMany(WorkingTimeAssignment::class);
     }
+
 }
