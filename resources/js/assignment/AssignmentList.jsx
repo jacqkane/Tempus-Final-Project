f6c1eac60dbf73bb4328e58969edfcd95a6c51c3
Radek Henych <henychr@gmail.com>
2024-03-22 23:08:55 +0100

    edit working

--- resources/js/assignment/AssignmentList.jsx
+++ resources/js/assignment/AssignmentList.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import '/resources/scss/assignment/AssignmentList.scss';
+import { useNavigate } from 'react-router-dom';
 
 
 
-export default function AssignmentList({ selectedDate }) {
+export default function AssignmentList({ selectedDate, refreshList, setEditFormId, dayEntries, setDayEntries }) {
+    const navigate = useNavigate();
+    // const [dayEntries, setDayEntries] = useState([]);
 
-    const [dayEntries, setDayEntries] = useState([]);
+
+
 
     // dayEntries[0] ?
     //     console.log(dayEntries[0].approval_status.status_name)
@@ -31,43 +35,86 @@ export default function AssignmentList({ selectedDate }) {
         }
     }
 
+
     useEffect(() => {
         getEntriesSelectedDay();
 
-    }, [selectedDate])
+    }, [selectedDate, refreshList])
+
+    const deleteEntry = async (id) => {
+        try {
+            const response = await axios.post('http://www.tempus.test/api/assignment/delete-entry', {
+                'id': id
+            });
+            // const response_data = await response.data;
+
+        } catch (error) {
+            switch (error.response.status) {
+                case 422:
+                    console.log('VALIDATION FAILED:', error.response.data.errors);
+                    break;
+                case 500:
+                    console.log('UNKNOWN ERROR', error.response.data);
+                    break;
+            }
+        }
+        getEntriesSelectedDay();
+
+    }
+
+
+    const handleDeleteOneEntry = (e) => {
+        e.preventDefault();
+        deleteEntry(e.target.value);
+
+    }
+
+    const handleEditOneEntry = (e) => {
+        setEditFormId(e.target.value)
+    }
 
 
     return (
 
         <div className="assignment-list">
             <table className="assignment-list__table">
-                <thead>
+                <thead >
                     <tr>
+                        <th></th>
                         <th>Project Number</th>
                         <th>RfQ Number</th>
                         <th>Type Code</th>
                         <th>Assigned Time</th>
                         <th>Comment</th>
                         <th>Approval Status</th>
+                        <th></th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    {console.log(dayEntries)}
-                    {console.log(dayEntries[0]?.id)}
                     {
-                        dayEntries[0]?.id ?
-                            dayEntries.map((entry) => (
+                        dayEntries[0]?.id &&
+
+                        dayEntries.map((entry) => {
+
+                            return (
                                 <tr key={entry.id}>
+                                    <td>
+                                        <button name="delete" value={entry.id} onClick={handleDeleteOneEntry}>-</button>
+                                    </td>
                                     <td>{entry.project.project_number}</td>
                                     <td>{entry.rfq.rfq_number}</td>
                                     <td>{entry.cost_group.cost_group_code}</td>
                                     <td>{entry.working_time_assigned}</td>
                                     <td>{entry.comment}</td>
                                     <td>{entry.approval_status.status_name}</td>
+                                    <td>
+                                        <button name="edit" value={entry.id} onClick={handleEditOneEntry}>/</button>
+                                    </td>
                                 </tr>
-                            ))
-                            : ''
+                            )
+                        })
+
                     }
 
                 </tbody >
